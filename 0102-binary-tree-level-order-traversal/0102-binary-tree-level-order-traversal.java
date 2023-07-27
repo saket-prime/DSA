320b2165db23b5cd781a105ea064ab0b6fecfea1
saket-prime <132968105+saket-prime@users.noreply.github.com>
2023-07-27 10:00:02 +0530

    Time: 3 ms (5.05%), Space: 44.2 MB (40.22%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   public List<List<Integer>> levelOrder(TreeNode root) {
+       Map<Integer, List<Integer>> listMap = new HashMap<>();
+        Queue<List<TreeNode>> nodes = new ArrayDeque<>();
+        int count = 0;
+
+        if (root != null){
+            List<TreeNode> list = List.of(root);
+            listMap.put(count, new ArrayList<>());
+            listMap.get(count).add(root.val);
+            nodes.add(list);
+        }
+        while(!nodes.isEmpty()){
+            count++;
+            listMap.put(count, new ArrayList<>());
+            List<TreeNode> first = nodes.poll();
+            List<TreeNode> temp = new ArrayList<>();
+            for (int i = 0; i < first.size(); i++){
+                if (first.get(i).left  != null) {
+                    temp.add(first.get(i).left);
+                    listMap.get(count).add(first.get(i).left.val);
+                }
+                if (first.get(i).right  != null) {
+                    temp.add(first.get(i).right);
+                    listMap.get(count).add(first.get(i).right.val);
+                }
+            }
+            if(!temp.isEmpty())nodes.add(temp);
+            if(listMap.get(count).isEmpty()) listMap.remove(count);
+            
+        }
+
+        return new ArrayList<>(listMap.values());
+    }
+}
