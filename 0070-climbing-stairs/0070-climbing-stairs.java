195adcf2008b8298d3e943ee9c2485390051c7a4
saket-prime <132968105+saket-prime@users.noreply.github.com>
2023-10-25 11:11:38 +0530

    Time: 0 ms (100%), Space: 38.8 MB (90.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int a = 0;
-        int b = 1;
-        for(int i = 0; i <= n; i++){
-          int c = b;
-          b = a + b;
-          a = c;
-        }
-        return a;
+      return fib(n, new HashMap<>());
+    }
+
+    public int fib(int n, Map<Integer, Integer> cache){
+      if(cache.containsKey(n)) return cache.get(n); 
+      int result = 0;
+      if(n <= 1) result = 1;
+      else{
+        result = fib(n-1, cache) + fib(n-2, cache);
+      }
+      cache.put(n, result);
+      return result;
     }
 }
